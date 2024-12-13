54697af0457f2049ddc0112a4fbba7c4b38f38a4
Ari <ari.basil.khan@gmail.com>
2024-12-13 15:29:23 -0500

    Moved index files and html.

--- script.js
+++ script.js
@@ -45,3 +45,24 @@ async function subscribe() {
     }
 }
 
+const firstWords = ["Discover", "Explore", "Embrace", "Uncover", "Achieve", "Ari"];
+const secondWords = ["Innovation", "Creativity", "Success", "Leadership", "Vision", "Khan"];
+const colors = ["#FF5733", "#33FF57", "#1964FA", "#FF33A8", "#FAD700", "#1964FA"];
+const fadeTextEl = document.getElementById('fade-text');
+
+let index = 0;
+
+function fadeEffect() {
+  fadeTextEl.style.opacity = 0;
+
+  setTimeout(() => {
+    fadeTextEl.innerHTML = `<span>${firstWords[index]}</span> <span style="color: ${colors[index]};">${secondWords[index]}</span>`;
+    fadeTextEl.style.opacity = 1;
+
+    index = (index + 1) % secondWords.length;
+
+    setTimeout(fadeEffect, 2000);
+  }, 1000);
+}
+
+fadeEffect();
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -8,13 +7,11 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 const db = mongoose.connection;
-db.once("open", () => console.log("MongoDB connected successfully!"));
 db.on("error", (err) => console.log("MongoDB connection error:", err));
 
 const emailSchema = new mongoose.Schema({
@@ -23,15 +20,16 @@ const emailSchema = new mongoose.Schema({
 
 const Subscriber = mongoose.model("Subscriber", emailSchema);
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/subscribe", async (req, res) => {
   const { email } = req.body;
   try {
     const newSubscriber = new Subscriber({ email });
     await newSubscriber.save();
-    res.status(201).json({ message: "Email added to the mailing list" });
+    res.status(201).json({ message: "Email added to the mailing list!" });
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
