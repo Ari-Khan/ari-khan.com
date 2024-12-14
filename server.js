b6a47f895ea3dad86b34aea40f7adc2bbcf4f895
Ari <ari.basil.khan@gmail.com>
2024-12-13 21:53:55 -0500

    Edited directories.

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected successfully!"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
-const db = mongoose.connection;
-db.on("error", (err) => console.log("MongoDB connection error:", err));
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +21,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "content")));
+
+app.use('/assets', express.static(path.join(__dirname, 'content/assets')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./content/main.html"));
@@ -32,4 +30,3 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
